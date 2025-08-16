ca244912896beac4d4e66c5445de0a0c0a9ee5f1
Jorge Luis <jlzamorap49@gmail.com>
2025-08-16 18:08:25 -0500

    Primer commit del proyecto DesafioForo

---
+++ src/main/java/com/aluraCursos/DesafioForo/DesafioForoApplication.java
@@ -0,0 +1,13 @@
+package com.aluraCursos.DesafioForo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioForoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioForoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluraCursos/DesafioForo/DesafioForoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluraCursos.DesafioForo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioForoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
